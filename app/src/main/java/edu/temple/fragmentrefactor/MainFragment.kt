4695284dad0db6d47ff83ada9ce98f235912b23b
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-10-23 15:33:39 -0400

    move functionality from main into fragment, use supportFragmentManager for dynamic instantiation at runtime

--- app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/MainFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MainFragment : Fragment() {
 
     override fun onCreateView(
@@ -18,7 +16,24 @@ class MainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+
+        val layout = inflater.inflate(R.layout.fragment_main, container, false)
+
+        val displayTextView = layout.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = layout.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = layout.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
+
+        return layout
     }
 
 }
